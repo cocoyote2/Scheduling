2d362fb58a230998e48aee13e70ed97829ad5f10
Dimitri Marain <maraindimitri@gmail.com>
2024-09-20 22:57:02 +0200

    Adding PrintDate functionnality + CSS

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
-let button = document.getElementById("Add")
-let form = document.getElementById("form")
+let calendar = document.getElementById("date");
+let chosenDate;
 
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-})
+calendar.addEventListener("change", () => {
+    let date = new Date(calendar.value);
+    chosenDate = date.toLocaleDateString();
+    PrintDate(chosenDate);
+});
 
-button.addEventListener("click", AddDay) 
-
-function AddDay(){
-    let begin = document.getElementById("begin").value;
-    let end = document.getElementById("end").value;
-    let Todaydate = new Date().toLocaleDateString()
-
-    let content = document.getElementById("tab-content");
-    let element = document.createElement("tr")
-    let date = document.createElement("td")
-    let demarrage = document.createElement("td");
-    let fin = document.createElement("td");
-
-    date.innerHTML = Todaydate;
-    demarrage.innerHTML = begin + "H";
-    fin.innerHTML = end + "H";
-
-    element.append(date);
-    element.append(demarrage);
-    element.append(fin);
-
-    content.append(element);
-}
-
-function CheckHour(hour){
+function PrintDate(date){
+    let content = document.getElementById("currDate");
+    content.innerHTML = date;
 }
