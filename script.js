60280009b4a611515154111a185b46d2c815b59f
Dimitri <maraindimitri@gmail.com>
2024-09-21 10:48:54 +0200

    Adding code to save data in localStorage

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 let calendar = document.getElementById("date");
+let submitButton = document.getElementById("add");
 let chosenDate;
+
+//localStorage.clear();
 
 calendar.addEventListener("change", () => {
     let date = new Date(calendar.value);
     chosenDate = date.toLocaleDateString();
-    PrintDate(chosenDate);
+
+    let content = JSON.parse(localStorage.getItem(chosenDate));
+
+    if (content != null) {
+        PrintContent(chosenDate, content);
+    }
 });
 
-function PrintDate(date){
-    let content = document.getElementById("currDate");
-    content.innerHTML = date;
+submitButton.addEventListener("click", () => {
+    let startHour = document.getElementById("begin").value;
+    let finishHour = document.getElementById("end").value;
+    let currDate = document.getElementById("currDate");
+
+    let shift = {
+        beginHour: startHour,
+        endHour: finishHour
+    }
+
+    jsonContent = JSON.stringify(shift);
+
+    localStorage.setItem(chosenDate, jsonContent);
+
+    currDate.innerHTML = chosenDate;
+});
+
+function PrintContent(date, content) {
+    let currDate = document.getElementById("currDate");
+    let startHour = document.getElementById("beginHour");
+    let finishHour = document.getElementById("endHour");
+
+    //Je dois ajouter les données du content à startHour et finishHour
+    currDate.innerHTML = date;
 }
